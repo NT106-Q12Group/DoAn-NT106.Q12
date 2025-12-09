f4aacb594dc47fc47e7afa0c2ba8a2895dc45e1b
Lenhat14810 <lengocminhnhat01@gmail.com>
2025-12-09 11:40:06 +0700

    Update UI

--- Doan/CaroGame/Properties/Resources.Designer.cs
+++ Doan/CaroGame/Properties/Resources.Designer.cs
@@ -253,6 +253,26 @@ namespace CaroGame.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap edit {
+            get {
+                object obj = ResourceManager.GetObject("edit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap editCaro {
+            get {
+                object obj = ResourceManager.GetObject("editCaro", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap file {
             get {
                 object obj = ResourceManager.GetObject("file", resourceCulture);
--- Doan/CaroGame/SignUp.Designer.cs
+++ Doan/CaroGame/SignUp.Designer.cs
@@ -216,6 +216,7 @@
             // 
             // pictureBox1
             // 
+            pictureBox1.BackgroundImage = Properties.Resources.CaroPicGame;
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
             pictureBox1.BorderStyle = BorderStyle.Fixed3D;
             pictureBox1.Location = new Point(192, 12);
